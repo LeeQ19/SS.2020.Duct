e1c10cb443f512dcd045764fae536323bbfc99ad
LeeQ19 <leekw1994@gmail.com>
2020-08-31 19:28:40 +0900

    Delete price.info

--- [Setup] DataSave.R
+++ [Setup] DataSave.R
@@ -4,12 +4,6 @@ search.data <- list()
 search.data[[format(Sys.time(), tz = "Asia/Seoul")]] <- data
 saveRDS(search.data, file = "DB/search.data.rds")
 
-# price.info
-info <- read.csv("DB/Price Info.csv", stringsAsFactors = FALSE, fileEncoding = "euc-kr")
-price.info <- list()
-price.info[[format(Sys.time())]] <- info
-saveRDS(price.info, file = "DB/price.info.rds")
-
 # cntrt.example
 example <- read.csv("DB/Contract Example.csv", stringsAsFactors = FALSE, fileEncoding = "euc-kr")
 saveRDS(example, file = "DB/cntrt.example.rds")
@@ -17,4 +11,4 @@ saveRDS(example, file = "DB/cntrt.example.rds")
 ### Test
 # rm(list = ls())
 # search.data <- readRDS("DB/search.data.rds")
-# price.info <- readRDS("DB/price.info.rds")
+# example <- readRDS("DB/cntrt.example.rds")
