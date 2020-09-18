edeb952adbafbfbc9ae17b2bf82526690029259b
LeeQ19 <37331313+LeeQ19@users.noreply.github.com>
2020-09-18 16:33:19 +0900

    Version 2.6.0

--- [Setup] DataSave.R
+++ [Setup] DataSave.R
@@ -1,5 +1,6 @@
 # search.data
 data <- read.csv("DB/Data.csv", stringsAsFactors = FALSE, fileEncoding = "euc-kr")
+data$품명 <- gsub("[[:blank:][:punct:]]", "", data$품명)
 search.data <- list()
 search.data[[format(Sys.time(), tz = "Asia/Seoul")]] <- data
 saveRDS(search.data, file = "DB/search.data.rds")
